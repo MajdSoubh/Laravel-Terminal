66b619a2a67adec1852546bd58e2c0ff6abaa3f4
MajdSoubh <majdsoubh53@gmail.com>
2024-10-20 18:51:10 +0300

    create terminal controller

---
+++ src/controllers/TerminalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Maso\WShell\controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Session;
+
+class TerminalController extends Controller
+{
+
+    public function index()
+    {
+        return view('wshell::terminal');
+    }
+
+    /**
+     * Run the command.
+     */
+    public function runCommand(Request $request)
+    {
+        $output = [];
+
+        $command = $request->input('command');
+        if (str_starts_with($command, 'cd '))
+        {
+            $pos = strpos($command, ' ');
+            $requestedDirectory = substr($command, $pos + 1);
+            $completeRequestedPath = $this->currentDirectory() . '/' . $requestedDirectory;
+
+            if (is_dir($completeRequestedPath))
+            {
+                chdir($completeRequestedPath);
+                $currentDirectory = getcwd();
+                Session::put('web-shell.directory', str_replace('\\', '/', $currentDirectory));
+            }
+            else
+            {
+                $output[] = 'System cannot find the path';
+            }
+        }
+
+        else
+        {
+
+            $currentDirectory = $this->currentDirectory();
+
+            chdir($currentDirectory);
+            exec($command  . ' 2>&1', $output);
+        }
+
+        return response()->json(['output' => $output, 'directory' => $this->currentDirectory()]);
+    }
+
+    public function getWorkingDirectory()
+    {
+        $workingDirectory = $this->currentDirectory();
+        return response()->json(['directory' => $workingDirectory]);
+    }
+
+    private function currentDirectory(): string
+    {
+        $currentDirectory = str_replace('\\', '/', Session::get('web-shell.directory', base_path()));
+        return $currentDirectory;
+    }
+}
