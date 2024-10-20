48e5392230feddf51b8857ffcd14b673b26b7bae
MajdSoubh <majdsoubh53@gmail.com>
2024-10-20 18:51:26 +0300

    create asset controller

---
+++ src/controllers/AssetController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Maso\WShell\controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class AssetController extends Controller
+{
+
+    /**
+     * Provide the asset to the client
+     */
+    public function getAsset(string $asset)
+    {
+
+        if (
+            !file_exists($path = __DIR__ . "/../resources/css/{$asset}")
+            && !file_exists($path = __DIR__ . "/../resources/js/{$asset}")
+            && !file_exists($path = __DIR__ . "/../resources/fonts/{$asset}")
+        )
+        {
+            abort(404);
+        }
+
+        $type = 'text/css';
+        if (str_ends_with($asset, '.js'))
+        {
+            $type = 'application/javascript';
+        }
+        else if (str_ends_with($asset, '.woff'))
+        {
+            $type = ' application/font-woff2';
+        }
+
+        return response()->file($path, [
+            'Content-Type' => $type,
+        ]);
+    }
+}
