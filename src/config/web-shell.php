92be4ab5ac02c6e15a625add0c2a5fef3358028c
MajdSoubh <majdsoubh53@gmail.com>
2024-10-22 14:21:59 +0300

    update header

--- src/config/web-shell.php
+++ src/config/web-shell.php
@@ -41,7 +41,7 @@ return [
         'showInteractiveWarning' => true,
         'prompt' => '$',
         'header' => [
-            'Welcome to <span class="ansi-blue" style="color: #61afef; font-weight:bold">Laravel Shell</span><span class="ansi-bright-yellow" style="color: #f3f99d;"> v' . \Jakyeru\LaravelShell\LaravelShellServiceProvider::VERSION . '</span>',
+            '<span class="ansi-blue" style="color: #61afef; font-weight:bold">Laravel Web Shell</span><span class="ansi-bright-yellow" style="color: #f3f99d;"> v' . \Maso\WebShell\WebShellServiceProvider::VERSION . '</span>',
             'Running Laravel <span class="ansi-bright-yellow" style="color: #f3f99d;"> v' . Illuminate\Foundation\Application::VERSION . '</span> (PHP <span class="ansi-bright-yellow" style="color: #f3f99d;">v' . PHP_VERSION . '</span>)',
 
         ]
